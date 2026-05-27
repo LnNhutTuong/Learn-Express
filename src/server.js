ac48563ffac42a22a75c3ee87a7eab14e14ab368
tuong <you@example.com>
2026-05-27 10:11:41 +0700

    move 'connection' into config->database

--- src/server.js
+++ src/server.js
@@ -3,23 +3,14 @@ const express = require('express') // commands
 const path = require('path')
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
+const connection = require('./config/database')
 const app = express() // app
 const port = process.env.PORT || 2202 // port
 const hostname = process.env.HOST_NAME
-const mysql = require('mysql2')
 
 configViewEngine(app);
 
 app.use('/abc',webRoutes);
-
-//test connection
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
 
 // simple query
 connection.query(
